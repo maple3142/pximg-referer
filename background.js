55fc506afc31ef36f95b8c4a77d3f5ff487dd677
maple3142 <kirby741852963@gmail.com>
2018-11-23 21:10:48 +0800

    update

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	details => {
-		const ref = details.requestHeaders.find(x => x.name === 'Referer')
-		const hasPixivReferer = ref && ref.value.includes('pixiv')
-		if (!hasPixivReferer) {
-			details.requestHeaders.push({
-				name: 'Referer',
-				value: 'https://www.pixiv.net/'
-			})
+		const idx = details.requestHeaders.findIndex(x => x.name.toLowerCase() === 'referer')
+		if (idx !== -1) {
+			details.requestHeaders.splice(idx, 1)
 		}
+		details.requestHeaders.push({
+			name: 'Referer',
+			value: 'https://www.pixiv.net/'
+		})
 		return {
 			requestHeaders: details.requestHeaders
 		}
 	},
-	{ urls: ['<all_urls>'] },
+	{ urls: ['https://i.pximg.net/*'] },
 	['requestHeaders', 'blocking']
 )
